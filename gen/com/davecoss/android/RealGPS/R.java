1a814cebc9124cf38537fcbf9f9cf1e098cb8f81
David Coss <david@davecoss.com>
2012-10-11 20:05:52 -0500

    Added a log feature that will save the coordinates to a log file in the format: lat, log

--- gen/com/davecoss/android/RealGPS/R.java
+++ gen/com/davecoss/android/RealGPS/R.java
@@ -15,8 +15,9 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int btn_save=0x7f070001;
         public static final int location_text=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int menu_settings=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -26,6 +27,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int btn_save=0x7f040004;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_main=0x7f040003;
--- src/com/davecoss/android/RealGPS/RealGPSMain.java
+++ src/com/davecoss/android/RealGPS/RealGPSMain.java
@@ -1,10 +1,17 @@
 package com.davecoss.android.RealGPS;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import com.davecoss.android.RealGPS.R;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
 import android.location.*;
 import android.content.Context;
@@ -12,11 +19,15 @@ import android.content.Context;
 public class RealGPSMain extends Activity {
 	private LocationManager locationManager;
 	LocationListener mlocListener;
+	double lat;
+	double lon;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+        lat = 1;
+        lon = 2;
 
         locationManager =
                 (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
@@ -36,6 +47,52 @@ public class RealGPSMain extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    public void save_location(View view)
+    {
+    	boolean mExternalStorageAvailable = false;
+    	boolean mExternalStorageWriteable = false;
+    	String state = Environment.getExternalStorageState();
+
+    	if (Environment.MEDIA_MOUNTED.equals(state)) {
+    	    // We can read and write the media
+    	    mExternalStorageAvailable = mExternalStorageWriteable = true;
+    	} else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+    	    // We can only read the media
+    	    mExternalStorageAvailable = true;
+    	    mExternalStorageWriteable = false;
+    	} else {
+    	    // Something else is wrong. It may be one of many other states, but all we need
+    	    //  to know is we can neither read nor write
+    	    mExternalStorageAvailable = mExternalStorageWriteable = false;
+    	}
+    	
+    	if(mExternalStorageAvailable && mExternalStorageWriteable)
+    	{
+    		File dir = getExternalFilesDir(null);
+    		if(!dir.exists())
+    		{
+    			if(!dir.mkdirs())
+    			{
+    				write_message("Could not make directory.");
+    				return;
+    			}
+    		}
+    		File file = new File(dir, "gps_log.txt");
+    		try {
+				OutputStream os = new FileOutputStream(file,true);
+    	        os.write((lat + ", " + lon+"\n").getBytes());
+    	        os.close();
+			
+    	    } catch (IOException e) {
+    	        write_message("ExternalStorage: Error writing " + file.getName() + "\n" + e.getMessage());
+    	    }
+    	}
+    	else
+    	{
+    		write_message("Cannot write file.");
+    	}
+    }
+    
     protected void onStop() {
         super.onStop();
         locationManager.removeUpdates(mlocListener);
@@ -54,18 +111,23 @@ public class RealGPSMain extends Activity {
         //startActivity(settingsIntent);
     }
     
+    private void write_message(String msg)
+    {
+    	TextView text = (TextView) findViewById(R.id.location_text);
+        text.setText(msg);
+    }
+    
     public class DRCLocationListener implements LocationListener
     {
-
     @Override
     public void onLocationChanged(Location loc)
     {
 
-    loc.getLatitude();
+    lat = loc.getLatitude();
 
-    loc.getLongitude();
+    lon = loc.getLongitude();
 
-    String bean = "My current location is: Lat = " + loc.getLatitude() + "Lon = " + loc.getLongitude();
+    String bean = "My current location is: Lat = " + lat + "Lon = " + lon;
 
     TextView text = (TextView) findViewById(R.id.location_text);
     text.setText(bean);
